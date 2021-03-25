34076dd12e4011f7fc145a3e92b4b79961c81edc
Dr Phill Edwards <phill@aux-quatre-vents-france.fr>
2021-03-25 10:02:39 +0100

    Added authorisation check

--- Server.py
+++ Server.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_api import status
+import base64
+import os
 import threading
 
 app = Flask(__name__)
@@ -19,9 +21,29 @@ def handleGet():
 @app.route('/', methods=['POST'])
 def handlePost():
     headers = dict(request.headers)
-    t = threading.Thread(target=worker, args=(request.json,))
-    t.start()
-    return '', status.HTTP_201_CREATED
+    print(headers)
+    code = status.HTTP_400_BAD_REQUEST
+    body = ''
+    
+    if validate(headers):
+        code = status.HTTP_204_NO_CONTENT
+        t = threading.Thread(target=worker, args=(request.json,))
+        t.start()
+    else:
+        body = jsonify(status='Authorization header missing or invalid')
+    return body, code
+
+def validate(headers):
+    status = False
+    password = os.environ.get('OKTA_HOOK_PASSWORD')
+    if password is not None and 'Authorization' in headers:
+        value = headers['Authorization']
+        parts = value.split()
+        if len(parts) == 2 and parts[0] == 'Basic':
+            credentials = 'admin:' + password
+            print(parts[1], base64.b64encode(str.encode(credentials)))
+            status = str.encode(parts[1]) == base64.b64encode(str.encode(credentials))
+    return status
 
 def worker(body):
     print(body)
